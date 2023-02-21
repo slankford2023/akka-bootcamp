251f3e693fa2c83e11b8d89ae015b1e7df57b489
Stephen Lankford <slankford@rwatsonassoc@gmail.com>
2023-02-21 11:27:10 -0800

    unit 1.2 done

---
+++ src/Unit-1/FSharp/DoThis/Messages.fs
@@ -0,0 +1,18 @@
+namespace WinTail
+
+[<AutoOpen>]
+module Messages =
+    
+    type Command =
+        | Start
+        | Continue
+        | Message of string
+        | Exit
+
+    type ErrorType =
+        | Null
+        | Validation
+
+    type InputResult =
+        | InputSuccess of string
+        | InputError of reason: string * errorType: ErrorType
--- src/Unit-1/FSharp/DoThis/Program.fs
+++ src/Unit-1/FSharp/DoThis/Program.fs
@@ -1,36 +1,19 @@
 ﻿open System
 open Akka.FSharp
-open Akka.FSharp.Spawn
 open Akka.Actor
 open WinTail
-
-let printInstructions () =
-    Console.WriteLine "Write whatever you want into the console!"
-    Console.Write "Some lines will appear as"
-    Console.ForegroundColor <- ConsoleColor.Red
-    Console.Write " red"
-    Console.ResetColor ()
-    Console.Write " and others will appear as"
-    Console.ForegroundColor <- ConsoleColor.Green
-    Console.Write " green! "
-    Console.ResetColor ()
-    Console.WriteLine ()
-    Console.WriteLine ()
-    Console.WriteLine "Type 'exit' to quit this application at any time.\n"
 
 [<EntryPoint>]
 let main argv = 
     // initialize an actor system
     let myActorSystem = System.create "MyActorSystem" (Configuration.load ())
-        
-    printInstructions ()
     
     // make your first actors using the 'spawn' function
     let consoleWriterActor = spawn myActorSystem "consoleWriterActor" (actorOf Actors.consoleWriterActor)  
     let consoleReaderActor = spawn myActorSystem "consoleReaderActor" (actorOf2 (Actors.consoleReaderActor consoleWriterActor))
 
     // tell the consoleReader actor to begin
-    consoleReaderActor <! Actors.Start
+    consoleReaderActor <! Start
 
     myActorSystem.WhenTerminated.Wait ()
     0
